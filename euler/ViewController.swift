b91f7b796f885803dea5174a3e20b343aee74196
Sanjay Guruprasad <sanjay.guruprasad@gmail.com>
2014-08-07 20:41:08 +0530

    Initial Commit

---
+++ euler/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  euler
+//
+//  Created by sanjaypojo on 07/08/14.
+//  Copyright (c) 2014 sanjaypojo. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+                                    
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+                                    
+    }
+
+
+}
+
---
+++ euler/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  euler
+//
+//  Created by sanjaypojo on 07/08/14.
+//  Copyright (c) 2014 sanjaypojo. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
